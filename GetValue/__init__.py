78bd97e0dff878f759a93061d1706c6a630077c8
p-zach <porterdzach@gmail.com>
2025-04-24 23:14:09 -0400

    Add comments

--- GetValue/__init__.py
+++ GetValue/__init__.py
@@ -6,16 +6,23 @@ import logging
 logger = logging.getLogger("azure.functions")
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
+    """
+    Get the value in the specified partition and row from the table named
+    VisitorTable in the Azure DB.
+    """
     logger.info("Function GetValue triggered")
     try:
+        # Connect to the table
         table_name = "VisitorTable"
         conn_str = os.environ["AZURE_STORAGE_CONNECTION_STRING"]
         service = TableServiceClient.from_connection_string(conn_str)
         table_client = service.get_table_client(table_name)
 
+        # Find entity via keys
         partition_key = req.params.get("partitionKey")
         row_key = req.params.get("rowKey")
 
+        # Get and return the value
         entity = table_client.get_entity(partition_key=partition_key, row_key=row_key)
         return func.HttpResponse(f"{entity['VALUE']}", status_code=200)
 
--- UpdateValue/__init__.py
+++ UpdateValue/__init__.py
@@ -6,13 +6,19 @@ import logging
 logger = logging.getLogger("azure.functions")
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
+    """
+    Update the value in the specified partition and row in the table named
+    VisitorTable in the Azure DB.
+    """
     logger.info("Function UpdateValue triggered")
     try:
+        # Connect to the table
         table_name = "VisitorTable"
         conn_str = os.environ["AZURE_STORAGE_CONNECTION_STRING"]
         service = TableServiceClient.from_connection_string(conn_str)
         table_client = service.get_table_client(table_name)
 
+        # Create entity from keys and new value
         data = req.get_json()
         partition_key = data.get("partitionKey")
         row_key = data.get("rowKey")
@@ -24,6 +30,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             "VALUE": new_value
         }
 
+        # Update entity
         table_client.upsert_entity(entity)
         return func.HttpResponse("Value updated", status_code=200)
 
